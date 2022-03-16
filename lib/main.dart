8b99e3bce6e76050d7ea57bdd10b9f62ab715de5
amXCVI <am.xcvi@gmail.com>
2022-03-16 11:33:02 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+final String assetName = 'assets/images/icon_loupe.svg';
+final Widget svg = SvgPicture.asset(
+  assetName,
+  color: Colors.red,
+);
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: '',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Pages'),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            SvgPicture.network('http://upload.wikimedia.org/wikipedia/commons/0/02/SVG_logo.svg'),
+            svg
+          ],
+        ),
+      ),
+    );
+  }
+}
